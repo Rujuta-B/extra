fa1551b96a1e3256e18ce0c3598626a7e22ac820
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-03-01 23:12:06 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Message from "./components/Message";
+function App() {
+  return (
+    <div className="App">
+     < Message />
+    </div>
+  );
+}
+
+export default App;
